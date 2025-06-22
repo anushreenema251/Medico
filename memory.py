98d0b4d1a311b0a6eb745cbc247f3d325ac938e5
anushreenema251 <anushreenema251@gmail.com>
2025-06-22 11:10:56 -0700

    Create memory.py

---
+++ memory.py
@@ -0,0 +1,10 @@
+from rag_module import retrieve_context
+# --- Step 5: RAG memory ---
+def rag1(memory):
+    return retrieve_context(memory)
+
+def database1(mock):
+    pass
+
+def letta(store1):
+    pass
